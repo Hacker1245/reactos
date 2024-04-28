eb2917939285af2a65c6c2b47e4492507ffff6e2
winesync <ros-dev@reactos.org>
2024-04-28 23:43:28 +0200

    [WINESYNC] adsldp: Fix a memory leak.
    Signed-off-by: Dmitry Timoshkov <dmitry@baikal.ru>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id 381b1396b9330b45782daad5a5bc1f2652b91b12 by Dmitry Timoshkov <dmitry@baikal.ru>

--- dll/win32/adsldp/adsldp.c
+++ dll/win32/adsldp/adsldp.c
@@ -1153,7 +1153,10 @@ static HRESULT WINAPI search_ExecuteSearch(IDirectorySearch *iface, LPWSTR filte
 
         props = heap_alloc((count + 1) * sizeof(props[0]));
         if (!props)
+        {
+            heap_free(ldap_ctx);
             return E_OUTOFMEMORY;
+        }
 
         for (i = 0; i < count; i++)
             props[i] = names[i];
